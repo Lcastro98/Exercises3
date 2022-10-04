d70b4609caafb23ab175e542c0ad1f479559e79d
Andrea Castro Manzano <andrea.castro@sofka.local>
2022-10-04 11:18:30 -0500

    Se crea la clase y los metodos solicitados

---
+++ src/main/java/com/bootcamp/Car.java
@@ -0,0 +1,13 @@
+package com.bootcamp;
+
+public class Car {
+    private int doors;
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void addDoor() {
+        this.doors++;
+    }
+}
--- src/main/java/com/bootcamp/Main.java
+++ src/main/java/com/bootcamp/Main.java
@@ -2,6 +2,16 @@ package com.bootcamp;
 
 public class Main {
     public static void main(String[] args) {
+        var result = sum(1,2,3);
+        System.out.println("El resultado de la suma es: " + result);
 
+        Car myCar = new Car();
+        myCar.addDoor();
+        myCar.addDoor();
+        System.out.println("El numero de puestas del carro es: " + myCar.getDoors());
+    }
+
+    public static int sum(int a, int b, int c) {
+        return a+b+c;
     }
 }
